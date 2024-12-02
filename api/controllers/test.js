f0086573e455ff0845e080e92bae98386cf1eca9
Juliard Actub <juliardactub@gmail.com>
2024-12-02 14:36:43 +0800

    prettier

--- api/controllers/test.js
+++ api/controllers/test.js
@@ -1,11 +1,10 @@
 module.exports = {
-
-    /**
-     * 
-     * @param {import("express").Request} req
-     * @param {import("express").Response} res 
-     */
-    testing: (req, res) => {
-        return res.end("hello");
-    }
+	/**
+	 *
+	 * @param {import("express").Request} req
+	 * @param {import("express").Response} res
+	 */
+	testing: (req, res) => {
+		return res.end("hello");
+	},
 };
